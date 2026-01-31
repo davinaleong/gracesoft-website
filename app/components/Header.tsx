288ec950703c0e910ac91b14c9ca208b42df3c34
Davina Leong <leong.shi.yun@gmail.com>
2026-01-31 18:53:33 +0800

    Corrected links.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,30 +1,31 @@
 import Image from "next/image"
+import Link from "next/link"
 import Logo from "./../assets/images/GS_IMGICON_2026.svg"
 
 export default function Header() {
   return (
     <header className="relative">
       <nav className="sticky top-0 flex items-center justify-between gap-4 p-4 text-xl">
-        <a href="#top" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
           <Image src={Logo} alt="GraceSoft Logo" height={16} />
           <span className="text-indigo-900 font-semibold">GraceSoft</span>
-        </a>
+        </Link>
 
         <ul className="flex justify-end gap-4 font-medium">
           <li>
-            <a href="#services" className="text-indigo-900 hover:text-indigo-700">
-              Services
-            </a>
+            <Link href="/" className="text-indigo-900 hover:text-indigo-700">
+              Home
+            </Link>
           </li>
           <li>
-            <a href="#about" className="text-indigo-900 hover:text-indigo-700">
+            <Link href="/about" className="text-indigo-900 hover:text-indigo-700">
               About
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#contact" className="text-indigo-900 hover:text-indigo-700">
+            <Link href="/contact" className="text-indigo-900 hover:text-indigo-700">
               Contact
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
