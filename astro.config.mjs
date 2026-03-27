c475505457e614bc1ef13be98454b724d21e94af
Davina Leong <leong.shi.yun@gmail.com>
2026-03-27 17:33:51 +0800

    Fixing deployment errors.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
